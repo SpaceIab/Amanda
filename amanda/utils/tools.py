d7828e5935757d8e7785509fb27a3e352240b33e
wertin <70998140+SpaceIab@users.noreply.github.com>
2021-09-13 13:04:04 +0400

    Create tools.py

---
+++ amanda/utils/tools.py
@@ -0,0 +1,95 @@
+import time
+
+from pyrogram.types import Message
+
+from assistant import bot, Config
+
+_BOT_ID = 0
+
+
+def time_formatter(seconds: float) -> str:
+    """ humanize """
+    minutes, seconds = divmod(int(seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "")
+    return tmp[:-2]
+
+
+async def extract_time(msg, time_val):
+    if any(time_val.endswith(unit) for unit in ('m', 'h', 'd')):
+        unit = time_val[-1]
+        time_num = time_val[:-1]  # type: str
+        if not time_num.isdigit():
+            await msg.reply("`Invalid vaxt tipi.`")
+            return
+
+        if unit == 'm':
+            bantime = int(time.time() + int(time_num) * 60)
+        elif unit == 'h':
+            bantime = int(time.time() + int(time_num) * 60 * 60)
+        elif unit == 'd':
+            bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
+        else:
+            await msg.reply("`ERROR`")
+            return
+        return bantime
+    else:
+        await msg.reply("`ERROR / TIME METNI DUZGUN DEYIL!!!`")
+        return
+
+
+def is_admin(chat_id: int, user_id: int, check_devs: bool = False) -> bool:
+    """ check user is admin or not in this chat """
+    if check_devs and is_dev(user_id):
+        return True
+    if chat_id not in Config.ADMINS:
+        return False
+    return user_id in Config.ADMINS[chat_id]
+
+
+def is_dev(user_id: int) -> bool:
+    """ returns user is dev or not """
+    return user_id in Config.DEV_USERS
+
+
+async def is_self(user_id: int) -> bool:
+    """ returns user is assistant or not """
+    global _BOT_ID  # pylint: disable=global-statement
+    if not _BOT_ID:
+        _BOT_ID = (await bot.get_me()).id
+    return user_id == _BOT_ID
+
+
+async def check_rights(chat_id: int, user_id: int, rights: str) -> bool:
+    """ check admin rights """
+    user = await bot.get_chat_member(chat_id, user_id)
+    if user.status == "member":
+        return False
+    if user.status == "administrator":
+        if getattr(user, rights, None):
+            return True
+        return False
+    return False
+
+
+async def check_bot_rights(chat_id: int, rights: str) -> bool:
+    """ check bot rights """
+    global _BOT_ID  # pylint: disable=global-statement
+    if not _BOT_ID:
+        _BOT_ID = (await bot.get_me()).id
+    bot_ = await bot.get_chat_member(chat_id, _BOT_ID)
+    if bot_.status == "administrator":
+        if getattr(bot_, rights, None):
+            return True
+        return False
+    return False
+
+
+async def sed_sticker(msg: Message):
+    """ send default sticker """
+    sticker = (await bot.get_messages('UserGeOt', 498697)).sticker.file_id
+    await msg.reply_sticker(sticker)
