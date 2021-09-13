d9a06c3ac75d960bb0152bf4898c6c53ee01b253
wertin <70998140+SpaceIab@users.noreply.github.com>
2021-09-13 12:56:03 +0400

    Create cus_filters.py

---
+++ amanda/cus_filters.py
@@ -0,0 +1,48 @@
+__all__ = ["auth_chats", "auth_users", "whitelist_chats"]
+
+import asyncio
+
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from . import Config, logging
+
+_LOG = logging.getLogger(__name__)
+_FETCHING = False
+
+
+async def _is_admin_or_dev(_, bot: Client, msg: Message) -> bool:
+    global _FETCHING  # pylint: disable=global-statement
+    if msg.chat.id not in Config.AUTH_CHATS:
+        return False
+    if not msg.from_user:
+        return False
+    if msg.from_user.id in Config.DEV_USERS:
+        return True
+    while _FETCHING:
+        _LOG.info("fetching tasklari gozlenilir ... (5s) !")
+        await asyncio.sleep(5)
+    if msg.chat.id not in Config.ADMINS:
+        _FETCHING = True
+        admins = []
+        _LOG.info("fetching datalari -- [%s] ...", msg.chat.id)
+        # pylint: disable=protected-access
+        async for c_m in bot.iter_chat_members(msg.chat.id):
+            if c_m.status in ("creator", "administrator"):
+                admins.append(c_m.user.id)
+        Config.ADMINS[msg.chat.id] = tuple(admins)
+        _LOG.info("data fetched -- [%s] !", msg.chat.id)
+        del admins
+        _FETCHING = False
+    return msg.from_user.id in Config.ADMINS[msg.chat.id]
+
+
+async def _is_chat_whitelist(_, __, msg: Message) -> bool:
+    if msg.chat.id in Config.WHITELIST_CHATS:
+        return True
+    return False
+
+
+auth_chats = filters.chat(list(Config.AUTH_CHATS))
+auth_users = filters.create(_is_admin_or_dev)
+whitelist_chats = filters.create(_is_chat_whitelist)
