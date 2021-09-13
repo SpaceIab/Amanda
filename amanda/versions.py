ab452c9673d7d0861fed72ce832d95a68f4b7604
wertin <70998140+SpaceIab@users.noreply.github.com>
2021-09-13 12:52:27 +0400

    Create versions.py

---
+++ amanda/versions.py
@@ -0,0 +1,11 @@
+# pylint: disable=missing-module-docstring
+#
+# Copyright (C) 2020 by wertin / thanks usrg
+# All rights reserved.
+
+from sys import version_info
+
+from pyrogram import __version__ as __pyro_version__  # mersi qardashim
+
+__amanda_version__ = "v1"
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
