c38ca3f2a48a5ac82e10f0b149219dc0bff3ccca
wertin <70998140+SpaceIab@users.noreply.github.com>
2021-09-13 13:02:02 +0400

    Create __init__.py

---
+++ amanda/__init__.py
@@ -0,0 +1,5 @@
+from .logger import logging  # niqqa
+from .config import Config  # niqqa
+from .bot import bot  # niqqa
+from . import cus_filters  # niqqa
+from .database import DB, save_data, load_data  # niqqa
